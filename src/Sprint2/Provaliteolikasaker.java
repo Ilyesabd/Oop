6a6f01ad345b3e3381a786ea46e3ecf63a146b7e
Ilyesabd <146825954+Ilyesabd@users.noreply.github.com>
2023-11-09 16:34:59 +0100

    Delete src/Sprint2/Provaliteolikasaker.java

--- src/Sprint2/Provaliteolikasaker.java
+++
@@ -1,9 +0,0 @@
-package Sprint2;
-
-import java.util.Locale;
-
-public class Provaliteolikasaker {
-    public static void main(String[] args) {
-        System.out.println(Locale.getDefault());
-    }
-}
